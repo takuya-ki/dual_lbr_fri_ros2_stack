5055b8b96367e80b23f20081199f2c52cefd9362
takuya-ki <taku8926@gmail.com>
2025-05-06 13:30:13 +0900

    add hello_moveit.launch.py

--- lbr_demos/lbr_moveit_py/setup.py
+++ lbr_demos/lbr_moveit_py/setup.py
@@ -1,3 +1,4 @@
+import glob
 from setuptools import setup
 
 package_name = "lbr_moveit_py"
@@ -9,6 +10,7 @@ setup(
     data_files=[
         ("share/ament_index/resource_index/packages", ["resource/" + package_name]),
         ("share/" + package_name, ["package.xml"]),
+        ("share/" + package_name + "/launch", glob.glob("launch/*.launch.py")),
     ],
     install_requires=["setuptools"],
     zip_safe=True,
